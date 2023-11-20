fe4b2da38152a7525fad773770a12032b406fe9f
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-11-20 16:46:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# first-3ds-rust-project
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use ctru::prelude::*;
+
+fn main() {
+    ctru::use_panic_handler();
+
+    let apt = Apt::new().unwrap();
+    let mut hid = Hid::new().unwrap();
+    let gfx = Gfx::new().unwrap();
+    let _console = Console::new(gfx.top_screen.borrow_mut());
+
+    println!("i like kissing boys meow mrrrp meow :3c");
+    println!("\x1b[29;16HPress Start to exit");
+
+    while apt.main_loop() {
+        gfx.wait_for_vblank();
+
+        hid.scan_input();
+        if hid.keys_down().contains(KeyPad::START) {
+            break;
+        }
+    }
+}
